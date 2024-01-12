5fa053136d15b73a5631f3b23dd2e9fdf7bb6301
htlin222 <hsieh.ting.lin@gmail.com>
2024-01-12 17:43:55 +0800

    Routine: Upload

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,14 +1,26 @@
 // @flow strict
-import React from 'react';
-import styles from './Meta.module.scss';
+import React from "react";
+import styles from "./Meta.module.scss";
 
 type Props = {
-  date: string
+  date: string,
+  title: string,
 };
 
 const Meta = ({ date }: Props) => (
-  <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
+  <div className={styles["meta"]}>
+    <p className={styles["meta__date"]}>
+      Published{" "}
+      {new Date(date).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}
+    </p>
+    <meta
+      property="og:image"
+      content="https://og-mlkgi530p-htlin222.vercel.app/api/blog?templateTitle=${title}"
+    />
   </div>
 );
 
