35c236c1f1b44a90e79e473e75ca4eb4ab654c65
htlin222 <hsieh.ting.lin@gmail.com>
2024-01-12 17:59:25 +0800

    Routine: Upload

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,23 @@
 // @flow strict
-import React from "react";
-import Helmet from "react-helmet";
-import type { Node as ReactNode } from "react";
-import { useSiteMetadata } from "../../hooks";
-import styles from "./Layout.module.scss";
+import React from 'react';
+import Helmet from 'react-helmet';
+import type { Node as ReactNode } from 'react';
+import { useSiteMetadata } from '../../hooks';
+import styles from './Layout.module.scss';
 
 type Props = {
   children: ReactNode,
   title: string,
   description?: string,
-  socialImage?: string,
+  socialImage?: string
 };
 
-const Layout = ({ children, title, description, socialImage = "" }: Props) => {
+const Layout = ({
+  children,
+  title,
+  description,
+  socialImage = ''
+}: Props) => {
   const { author, url } = useSiteMetadata();
   const metaImage = socialImage || author.photo;
   const metaImageUrl = url + metaImage;
@@ -24,11 +29,7 @@ const Layout = ({ children, title, description, socialImage = "" }: Props) => {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta property="og:site_name" content={title} />
-
-        <meta
-          property="og:image"
-          content="https://og-mlkgi530p-htlin222.vercel.app/api/blog?templateTitle=${metaImageUrl}"
-        />
+        <meta property="og:image" content={metaImageUrl} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
@@ -39,8 +40,4 @@ const Layout = ({ children, title, description, socialImage = "" }: Props) => {
   );
 };
 
-<meta
-  property="og:image"
-  content="https://og-mlkgi530p-htlin222.vercel.app/api/blog?templateTitle=${metaImageUrl}"
-/>;
 export default Layout;
--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,22 +1,14 @@
 // @flow strict
-import React from "react";
-import styles from "./Meta.module.scss";
+import React from 'react';
+import styles from './Meta.module.scss';
 
 type Props = {
-  date: string,
-  title: string,
+  date: string
 };
 
 const Meta = ({ date }: Props) => (
-  <div className={styles["meta"]}>
-    <p className={styles["meta__date"]}>
-      Published{" "}
-      {new Date(date).toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      })}
-    </p>
+  <div className={styles['meta']}>
+    <p className={styles['meta__date']}>Published {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
   </div>
 );
 
